5df0bacc5524489ed0c6a418d91e798f9acbf2c1
Subhankar <Subhankar@Subhankar-PC>
2016-10-30 23:23:29 +0530

    initial commit

---
+++ src/main/java/com/example/BootDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class BootDemoApplication  extends WebSecurityConfigurerAdapter{
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+	    return principal;
+	  }
+	  
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoApplication.class, args);
+	}
+	
+}
